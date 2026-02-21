2e762640c3ae87267128d1016a6c2c4855b478ed
MOminRifat <mominrifat8@gmail.com>
2026-02-21 19:34:26 +0600

    miss.cpp

---
+++ miss.cpp
@@ -0,0 +1,43 @@
+// Created By Momin_Rifat
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+template <typename T> using oder_set = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    long long int n;
+    cin >> n;
+    long long int sum = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+    cout << ((n * (n + 1)) / 2) - sum << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int tc = 1;
+    //cin >> tc;
+    while(tc--)
+    {
+       solve();
+    }
+    return 0;
+}
