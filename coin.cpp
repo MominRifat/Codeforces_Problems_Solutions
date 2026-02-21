6752377aae02c28d90d8da28193d8d323030ff15
MOminRifat <mominrifat8@gmail.com>
2026-02-21 19:24:06 +0600

    save

--- coin.cpp
+++ coin.cpp
@@ -23,6 +23,10 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
     int tc = 1;
     cin >> tc;
     while(tc--)
