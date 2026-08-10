870bfda067a4973b2eeaa7c018e365af0a5cf8f9
Momin_Rifat <mominrifat8@gmail.com>
2026-08-10 17:09:32 +0600

    add

--- palindrom.cpp
+++ palindrom.cpp
@@ -14,37 +14,46 @@ void solve()
 {
     string s;
     cin >> s;
+
+    // Count frequency of each character
     map<char,int>mp;
     for(auto x : s) mp[x]++;
+
+    // Count how many characters have odd frequency
+    // For a palindrome, at most one character can have odd frequency
     int cnt = 0;
-    char c;
-    bool flag = false;
+    char c;  // Store the character with odd frequency (if any)
+    bool flag = false;  // flag = true means there is an odd frequency character
     for (auto x : mp)
     {
-        if(x.second % 2 == 1) 
-        {                                                          
+        if(x.second % 2 == 1)
+        {
             cnt++;
-            c = x.first; 
-            flag = true;                     
+            c = x.first;
+            flag = true;
         }
+        // If more than one character has odd frequency, palindrome is impossible
         if(cnt == 2)
         {
             cout << "NO SOLUTION" << endl;
             return;
         }
     }
+    // Print first half of palindrome (left side in sorted order)
     for(auto x : mp)
     {
-        if(flag) if(x.first == c) continue;
+        if(flag) if(x.first == c) continue;  // Skip the middle odd character
         for(int i = 1; i <= x.second / 2; i++)
         {
             cout << x.first;
         }
     }
+    // Print the middle character (if exists)
     if(flag) for(int i = 1; i <= mp[c]; i++) cout << c;
+    // Print second half of palindrome (right side in reverse sorted order)
     for(auto x = mp.rbegin(); x != mp.rend(); x++)
     {
-        if(flag) if (x->first == c) continue;
+        if(flag) if (x->first == c) continue;  // Skip the middle odd character
         for(int i = 1; i <= x->second / 2; i++)
         {
             cout << x->first;
